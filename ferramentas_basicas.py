7e0412497120c6de6dd68e2b10c94027f538f495
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2021-06-01 23:06:11 -0300

    adicionar função de normalização de vetor

--- ferramentas_basicas.py
+++ ferramentas_basicas.py
@@ -1,6 +1,17 @@
+from math import sqrt
+
 def norma(vetor):
     a = vetor.x ** 2
     b = vetor.y ** 2
     c = vetor.z ** 2
 
     return sqrt(a+b+c)
+
+def normalize(vetor):
+    fator_de_correcao = norma(vetor)
+
+    a = vetor.x / fator_de_correcao
+    b = vetor.y / fator_de_correcao
+    c = vetor.z / fator_de_correcao
+
+    return Vetor(a, b, c)
