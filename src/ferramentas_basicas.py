708aab88cdce9383575a93b4e91d4a40d89a2029
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2021-06-02 15:04:53 -0300

    adicionar calculo do produto vetorial

--- src/ferramentas_basicas.py
+++ src/ferramentas_basicas.py
@@ -27,3 +27,10 @@ def projecao(v1, v2):
     escalar = produtoEscalar(v1, v2) / produtoEscalar(v2, v2)
 
     return Vetor(escalar * v2.x, escalar * v2.y, escalar * v2.z)
+
+def produtoVetorial(v1, v2):
+    x = (v1.y * v2.z) - (v1.z * v2.y)
+    y = (v1.z * v2.x) - (v1.x * v2.z)
+    z = (v1.x * v2.y) - (v1.y * v2.x)
+
+    return Vetor(x, y, z)
