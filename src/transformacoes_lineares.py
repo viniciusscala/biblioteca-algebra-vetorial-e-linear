7b1d3c3505262936fc22249f6c444d733171deaa
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2021-06-09 20:09:13 -0300

    adicionar reflexao com relação a reta

--- src/transformacoes_lineares.py
+++ src/transformacoes_lineares.py
@@ -1,4 +1,8 @@
 from estruturas import Vetor
+from ferramentas_basicas import reflexao as reflexao_vetor
+
+def reflexao_reta(vetor, vetorDiretor):
+    return reflexao_vetor(vetor, vetorDiretor)
 
 def reflexao(vetor):
     return Vetor(-1*vetor.x, -1*vetor.y, -1*vetor.z)
