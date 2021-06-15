206672e568eeb250213c02952713fad0786daccf
lss8 <38145232+lss8@users.noreply.github.com>
2021-06-15 19:34:26 -0300

    Adição das funções de Cisalhamento

--- src/transformacoes_lineares.py
+++ src/transformacoes_lineares.py
@@ -9,3 +9,56 @@ def reflexao(vetor):
 
 def deformacao(vetor, fatorX, fatorY, fatorZ):
     return Vetor(vetor.x * fatorX, vetor.y * fatorY, vetor.z * fatorZ)
+
+def cisalhamento(vetor, eixos, fator):
+    if (eixos == "XY")
+        x = vetor.x + fator*vetor.y
+        y = vetor.y
+        z = vetor.z
+
+    if (eixos == "XZ")
+        x = vetor.x + fator*vetor.z
+        y = vetor.y
+        z = vetor.z
+
+    if (eixos == "YX")
+        y = vetor.y + fator*vetor.x
+        x = vetor.x
+        z = vetor.z
+
+    if (eixos == "YZ")
+        y = vetor.y + fator*vetor.z
+        x = vetor.x
+        z = vetor.z
+
+    if (eixos == "ZX")
+        z = vetor.z + fator*vetor.x
+        y = vetor.y
+        x = vetor.x
+
+    if (eixos == "ZY")
+        z = vetor.z + fator*vetor.y
+        y = vetor.y
+        x = vetor.x
+
+    vetorCisalhado = Vetor(x, y, z)
+    return vetorCisalhado
+
+def cisalhamento(vetor, eixos, fator1, fator2):
+    if (eixos == "XYZ")
+        x = vetor.x + fator1*vetor.y + fator2*vetor.z
+        y = vetor.y
+        z = vetor.z
+
+    if (eixos == "YZX")
+        y = vetor.y + fator1*vetor.z + fator2*vetor.x
+        x = vetor.x
+        z = vetor.z
+
+    if (eixos == "ZXY")
+        z = vetor.z + fator1*vetor.x + fator2*vetor.y
+        y = vetor.y
+        x = vetor.x
+
+    vetorCisalhado = Vetor(x, y, z)
+    return vetorCisalhado
